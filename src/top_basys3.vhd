2750fed159ac217371c534bdbaa11cb45f5f4988
ObAkinyemi <c28obaloluwa.akinyemi@afacademy.af.edu>
2025-05-02 10:07:09 -0600

    Bernard faught some errors form my copy pasta and fixed my component declarations.

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -23,8 +23,8 @@
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
-
-
+ 
+ 
 entity top_basys3 is
     port(
         -- inputs
@@ -32,7 +32,6 @@ entity top_basys3 is
         sw      :   in std_logic_vector(7 downto 0); -- operands and opcode
         btnU    :   in std_logic; -- reset
         btnC    :   in std_logic; -- fsm cycle
-        
         -- outputs
         led :   out std_logic_vector(15 downto 0);
         -- 7-segment display segments (active-low cathodes)
@@ -41,42 +40,38 @@ entity top_basys3 is
         an  :   out std_logic_vector(3 downto 0)
     );
 end top_basys3;
-
+ 
 architecture top_basys3_arch of top_basys3 is 
-  
 	-- declare components and signals
-
+ 
 --components  
-component sevenseg_decoder is
-        port (
+    component sevenseg_decoder is
+            port (
             i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
             o_seg_n : out STD_LOGIC_VECTOR (6 downto 0)
-        );
-    end component sevenseg_decoder;
-    
-    component elevator_controller_fsm is
-		Port (
-            i_clk        : in  STD_LOGIC;
-            i_reset      : in  STD_LOGIC;
-            is_stopped   : in  STD_LOGIC;
-            go_up_down   : in  STD_LOGIC;
-            o_floor : out STD_LOGIC_VECTOR (3 downto 0)		   
-		 );
-	end component elevator_controller_fsm;
-	
-	component twos_comp is
-	   Port (
+            );
+        end component sevenseg_decoder;
+    component controller_fsm is
+            port (
+            i_reset : in  STD_LOGIC;
+            i_adv  : in  STD_LOGIC;
+            o_cycle : out STD_LOGIC_VECTOR (3 downto 0)		   
+	        );
+	   end component controller_fsm;
+ 
+    component twos_comp is
+	       port (
 	       i_bin   : in STD_LOGIC_VECTOR(7 downto 0);
 	       o_sign  : out STD_LOGIC;
-	       o_hund  : out STD_LOGIC;
-	       o_tens  : out STD_LOGIC;
-	       o_ones  : out STD_LOGIC
-	   );
-	end component twos_comp;
-	
-	component TDM4 is
-		generic ( constant k_WIDTH : natural  := 4); -- bits in input and output
-        Port ( i_clk		: in  STD_LOGIC;
+	       o_hund  : out STD_LOGIC_VECTOR(3 downto 0);
+	       o_tens  : out STD_LOGIC_VECTOR(3 downto 0);
+	       o_ones  : out STD_LOGIC_VECTOR(3 downto 0)
+	       );
+	   end component twos_comp;
+ 
+    component TDM4 is
+		  generic ( constant k_WIDTH : natural  := 4); -- bits in input and output
+          port(i_clk		: in  STD_LOGIC;
                i_reset		: in  STD_LOGIC; -- asynchronous
                i_D3 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
                i_D2 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
@@ -84,39 +79,36 @@ component sevenseg_decoder is
                i_D0 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
                o_data		: out STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
                o_sel		: out STD_LOGIC_VECTOR (3 downto 0)	-- selected data line (one-cold)
-	   );
-    end component TDM4;
-    
-    component ALU is
-        Port (
-            i_A     : in std_logic_vector (7 downto 0);
-            i_B     : in std_logic_vector (7 downto 0);
-            i_op    : in std_logic_vector (2 downto 0);
-            o_flags : out std_logic_vector(3 downto 0);
-            o_result: out std_logic_vector(7 downto 0)
-        );
-    end component ALU;
-    
+	       );
+        end component TDM4;
     component clock_divider is
-        generic ( constant k_DIV : natural := 2	); -- How many clk cycles until slow clock toggles
+            generic ( constant k_DIV : natural := 2	); -- How many clk cycles until slow clock toggles
                                                    -- Effectively, you divide the clk double this 
                                                    -- number (e.g., k_DIV := 2 --> clock divider of 4)
-        port ( 	i_clk    : in std_logic;
-                i_reset  : in std_logic;		   -- asynchronous
-                o_clk    : out std_logic		   -- divided (slow) clock
-        );
-    end component clock_divider;
-
+            port (  i_clk    : in std_logic;
+                    i_reset  : in std_logic;		   -- asynchronous
+                    o_clk    : out std_logic		   -- divided (slow) clock
+            );
+        end component clock_divider;
+ 
+    component ALU is
+            Port ( i_A : in STD_LOGIC_VECTOR (7 downto 0);
+                i_B : in STD_LOGIC_VECTOR (7 downto 0);
+                i_op : in STD_LOGIC_VECTOR (2 downto 0);
+                o_result : out STD_LOGIC_VECTOR (7 downto 0);
+                o_flags : out STD_LOGIC_VECTOR (3 downto 0)
+            );
+        end component ALU;
+ 
+ 
 -- hot singles
-
-
+ 
+ 
+ 
 begin
 	-- PORT MAPS ----------------------------------------
-
-	
+ 
 	
 	-- CONCURRENT STATEMENTS ----------------------------
-	
-	
-	
+
 end top_basys3_arch;
