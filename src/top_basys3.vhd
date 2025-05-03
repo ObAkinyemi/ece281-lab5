7795d54edf356f388d16a8eb69bfe7446d0b9b5d
ObAkinyemi <c28obaloluwa.akinyemi@afacademy.af.edu>
2025-05-02 19:26:35 -0600

    mux logic not working properly. [Synth 8-690] width mismatch in assignment; target has 8 bits, source has 4 bits - error message. Source is w_cycle (I think??) target is i_bin (I think???). Fix mux logic later.

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -105,7 +105,7 @@ architecture top_basys3_arch of top_basys3 is
  
  
 -- hot singles
-    signal w_data, w_cycle, w_sel   : std_logic_vector(3 downto 0);
+    signal w_data, w_cycle, w_sel, w_flags   : std_logic_vector(3 downto 0);
     signal master_reset : std_logic;
     signal w_result, w_i_A, w_i_B : std_logic_vector(7 downto 0);
     signal w_D3, w_D2, w_D1, w_D0 : std_logic_vector(3 downto 0);
@@ -157,11 +157,12 @@ begin
 	-- CONCURRENT STATEMENTS ----------------------------
     master_reset <= btnL or btnU;
     led(3 downto 0) <= w_cycle;
+    led(15 downto 12) <= w_flags;
     
---    mux logic
-    with w_cycle select
-        w_bin <= w_i_A when "0010",
-                 w_i_B when "0100",
-                 w_result when "1000",
-                 "0001" when others;
+--    mux logic not working
+--    with w_cycle select
+--        w_bin <= w_i_A when "0010",
+--                 w_i_B when "0100",
+--                 w_result when "1000",
+--                 "0001" when others;
 end top_basys3_arch;
